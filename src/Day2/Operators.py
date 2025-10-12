b6263947e280f4de88efd1e26d0b1595bded329e
Lenovo <anil.tsip07@gmail.com>
2025-10-12 10:33:23 +0530

    a new line of code is added.

---
+++ src/Day2/Operators.py
@@ -0,0 +1,28 @@
+#Arithmetic operators
+# a=5
+# b=2
+# print(a+b) #7
+# print(a-b) #3
+# print(a*b) #10
+# print(a/b) #2.5
+# print(a//b) #2
+# print(a%b) #1
+# print(a**b) #25
+
+#Relational Operators/comparision operators
+#they return always boolean value true or false
+#<,>, <= ,>= ,!=,==
+# a=5
+# b=10
+# print(a>b) #False
+# print(a==b) #False
+# print(a!=b) #True
+# print(a<b) #True
+# print(a<=b) #True
+# print(a>=b) #False
+
+
+
+
+
+
