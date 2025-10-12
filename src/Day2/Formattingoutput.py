4c5a80f35d9f09f17c14e44003f320f14803e6d0
Lenovo <anil.tsip07@gmail.com>
2025-10-12 11:41:24 +0530

    a new line of code is added.

---
+++ src/Day2/Formattingoutput.py
@@ -0,0 +1,11 @@
+name, age, sal = "john", 65, 5000
+# print(name,age,sal)
+
+# print("name is: ",name)
+# print("age is: ",age)
+# print("sal is: ",sal)
+
+# print("name is:%s,age is: %d,and sal is :%g" % (name, age, sal))
+
+print("name is:{},age is: {},and sal is :{}" .format(name,age,sal))
+print("age is:{},name is: {},and sal is :{}" .format(age,name,sal))
---
+++ src/Day2/Takinginputfromuser.py
@@ -0,0 +1,13 @@
+# num1=input("Enter first number:")
+# num2=input("Enter second number:")
+# print(type(num1))
+# print(type(num2))
+# sum=num1+num2
+# print(sum)
+
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+print(type(num1))
+print(type(num2))
+sum=num1+num2
+print(sum)
