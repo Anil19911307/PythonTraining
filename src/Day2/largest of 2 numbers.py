16cd5eff71de1bc1195084f8482cc56853089548
Lenovo <anil.tsip07@gmail.com>
2025-10-14 12:07:40 +0530

    a new line of code is added.

---
+++ src/Day2/largest of 2 numbers.py
@@ -0,0 +1,24 @@
+# Print largest of  two numbers
+# a,b=10,20
+# print("a is largest") if a >b else print("b is largest")
+
+# print largest of 3 numbers
+# a,b,c=10,20,30
+# if a>b and a >c:
+#     print("a is largest")
+# elif b>a and b>c:
+#     print("b is largest")
+# else:
+#     print("c is largest")
+
+#print given number is positive or negative
+
+# num1=int(input("Enter first number: "))
+# if num1>0:
+#     print("number is positive")
+# elif num1<0:
+#     print("number is negative")
+# else:
+#     print("number is zero")
+
+
