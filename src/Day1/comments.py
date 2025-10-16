264f9ddcbc0c7eea5b92ec7f56a923db78321db0
Lenovo <anil.tsip07@gmail.com>
2025-10-16 12:58:56 +0530

    a new line of code is added.

--- src/Day1/comments.py
+++ src/Day1/comments.py
@@ -8,9 +8,9 @@
 # this is my first python program
 # this is my first python program
 
-'''this is my first python program
+"""this is my first python program
 this is my first python program
 this is my first python program
-this is my first python program'''
+this is my first python program"""
 
 print("I am working with python")
--- src/Day3/stringsdemo.py
+++ src/Day3/stringsdemo.py
@@ -85,17 +85,32 @@
 
 # converting strings
 
-s = "String in PYTHON"
-s1 = s.capitalize()
-print(s1)  # String in python
-s2 = s.title()
-print(s2)  # String In Python
-s3 = s.lower()
-print(s3)  # string in python
-s4 = s.upper()
-print(s4)  # STRING IN PYTHON
-s5 = s.swapcase()
-print(s5)  # sTRING IN python
-s6 = s.replace("in", "on")
-print(s6)  # Strong on PYTHON
-print(s)  # String in PYTHON
+# s = "String in PYTHON"
+# s1 = s.capitalize()
+# print(s1)  # String in python
+# s2 = s.title()
+# print(s2)  # String In Python
+# s3 = s.lower()
+# print(s3)  # string in python
+# s4 = s.upper()
+# print(s4)  # STRING IN PYTHON
+# s5 = s.swapcase()
+# print(s5)  # sTRING IN python
+# s6 = s.replace("in", "on")
+# print(s6)  # Strong on PYTHON
+# print(s)  # String in PYTHON
+
+# s="welcome"
+# rev_str=""
+# for i in s:
+#     rev_str=i+rev_str
+# print("Reversed string is:",rev_str)
+
+# s='welcome'
+# for i in s:
+#     print(i,end=' ')
+
+# s='welcome'
+# rev_str=s[::-1]
+# print(s)
+# print(rev_str)
